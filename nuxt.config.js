b5ba0079c1a1cbae4b118e3deebaf9fcad2d2e00
Pierre Rolland <roll.pierre@gmail.com>
2020-12-07 21:45:40 +0100

    Made fonts static

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@assets/css/materialdesignicons.min.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -50,4 +51,10 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
+
+  vuetify: {
+    icons: {
+      iconfont: 'mdi'
+    }
+  }
 }
