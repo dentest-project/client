66666b850022278ba8ab8de4c30aa66f95d904ef
Pierre Rolland <roll.pierre@gmail.com>
2021-05-11 16:36:43 +0200

    Changed copy button label

--- components/buttons/CopyButton.vue
+++ components/buttons/CopyButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn @click="e => $emit('click', e)" :color="$colors.quaternary" fab dark x-small title="Move up">
+  <v-btn @click="e => $emit('click', e)" :color="$colors.quaternary" fab dark x-small title="Copy">
     <v-icon>mdi-content-copy</v-icon>
   </v-btn>
 </template>
