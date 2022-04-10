22592366b86164c3bbd5319f8d9caadb79ec5c4e
Pierre Rolland <roll.pierre@gmail.com>
2022-04-10 18:09:22 +0200

    Hide tags filter by default

--- components/StepsFilters.vue
+++ components/StepsFilters.vue
@@ -1,16 +1,23 @@
 <template>
-  <v-list-item class="steps-filters">
-    <tag-chip
-      v-for="tag in tags"
-      :key="tag.id"
-      :tag="tag"
-      :outlined="!isTagSelected(tag)"
-      @click="onTagClicked"
-    />
-    <v-list-item-content v-if="tags.length === 0" class="no-tags">
-      Add tags to your steps to filter them
-    </v-list-item-content>
-  </v-list-item>
+  <v-list-group :value="false" prepend-icon="mdi-filter">
+    <template v-slot:activator>
+      <v-list-item-content>
+        <v-list-item-title>Filters</v-list-item-title>
+      </v-list-item-content>
+    </template>
+    <v-list-item class="steps-filters">
+      <tag-chip
+        v-for="tag in tags"
+        :key="tag.id"
+        :tag="tag"
+        :outlined="!isTagSelected(tag)"
+        @click="onTagClicked"
+      />
+      <v-list-item-content v-if="tags.length === 0" class="no-tags">
+        Add tags to your steps to filter them
+      </v-list-item-content>
+    </v-list-item>
+  </v-list-group>
 </template>
 
 <script lang="ts">
