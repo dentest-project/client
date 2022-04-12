4f694158da64af33c26f8604731c8d11811d050b
Pierre Rolland <roll.pierre@gmail.com>
2022-04-12 14:18:09 +0200

    Added Nuxt support for breadcrumbs

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -7,6 +7,9 @@
       <template v-slot:divider>
         <v-icon>mdi-chevron-right</v-icon>
       </template>
+      <template v-slot:item="{ item }">
+        <v-breadcrumbs-item :href="item.href" :nuxt="true">{{ item.text }}</v-breadcrumbs-item>
+      </template>
     </v-breadcrumbs>
   </div>
 </template>
